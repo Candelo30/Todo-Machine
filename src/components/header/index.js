147ed8ae1eea377b68bf930645203d619daee0ea
Candelo30 <85467508+Candelo30@users.noreply.github.com>
2022-11-08 17:58:58 -0500

    1 file modified

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,7 +11,7 @@ function Header() {
   useEffect(() => {
     document
       .getElementsByTagName('body')[0]
-      .setAttribute('class', localStorage.getItem('theme'));
+      .setAttribute('className', localStorage.getItem('theme'));
   }, [checked]);
 
   const toggleThemeChange = () => {
