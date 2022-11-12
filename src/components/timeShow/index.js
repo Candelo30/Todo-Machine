4e4b3cca2e0438ab57f3a1fdb9ab4637ffcceb7e
Candelo30 <85467508+Candelo30@users.noreply.github.com>
2022-11-12 09:49:15 -0500

    2 files modified

--- src/components/timeShow/index.js
+++ src/components/timeShow/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './timeShow.css';
 import { FcClock } from 'react-icons/fc';
 
-function Tick(props) {
+function Tick() {
+  const h1 = useRef();
+  const ti = () => {
+    const fechahora = new Date();
+    const hora = fechahora.getHours();
+    const minuto = fechahora.getMinutes();
+    const segundo = fechahora.getSeconds();
+    return `${hora}:${minuto}:${segundo}`;
+  };
+  useEffect(() => {
+    const cl = setInterval(() => {
+      h1.current.innerHTML = `${ti()}`;
+    }, 1000);
+    return () => clearInterval(cl);
+  }, []);
   return (
     <div className="tick">
       <FcClock className="tick-icon__clock" />
-      <h2>{props.date.toLocaleTimeString()}.</h2>
+      <h2 ref={h1}>{ti()}</h2>
     </div>
   );
 }
