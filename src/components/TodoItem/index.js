9374ce36337effab186a8df6b6f0eabda9efc2b2
Candelo30 <85467508+Candelo30@users.noreply.github.com>
2022-11-18 19:26:10 -0500

    2 files modified

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './TodoItem.css';
 import { BsCheckLg, BsXLg } from 'react-icons/bs';
 
 function TodoItem(props) {
+  const ti = () => {
+    const fechahora = new Date();
+    const hora = fechahora.getHours();
+    const minuto = fechahora.getMinutes();
+    return `${hora}:${minuto}`;
+  };
   return (
-    <li
-      className={`TodoItem ${props.completed && 'TodoItems-completed'}`}
-    >
+    <li className={`TodoItem ${props.completed && 'TodoItems-completed'}`}>
       <BsCheckLg
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
         onClick={props.onComplete}
       />
-
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
       <BsXLg className="Icon Icon-delete" onClick={props.onDelete} />
+      <div className="">
+        <h2>{ti()}</h2>
+      </div>
     </li>
   );
 }
