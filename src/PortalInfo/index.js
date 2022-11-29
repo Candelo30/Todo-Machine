7ced1e665c3810d4ad5474836c9f92ab1e6626ea
Candelo30 <85467508+Candelo30@users.noreply.github.com>
2022-11-28 20:08:06 -0500

    1 file modified

--- src/PortalInfo/index.js
+++ src/PortalInfo/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 // Necesitamos ReactDOM para renderizar nuestro modal en el DOM
 import ReactDOM from 'react-dom';
 
-function Modalcolor({ children }) {
+function Modalinfo() {
   // ReactDom tiene este método para crear portales
   return ReactDOM.createPortal(
-    <div className="ModalBackground">{children}</div>,
+    <div className="ModalBackground">
+      <div>
+        <h1>Todo Machine</h1>
+        <p>
+          Una lista de pendientes sencilla, tiene las funciones normales, como
+          agregar, eliminara, y completar tus tareas, también te damos la opción
+          de poder filtras tus pendientes. Así que te invito a probar esta app.
+        </p>
+      </div>
+    </div>,
     document.getElementById('modalinfo')
   );
 }
 
-export { Modalcolor };
+export { Modalinfo };
